767d4ba3f6115d27a64e60e0e8711e4078658838
xvepkj <pkjrockzz@gmail.com>
2021-08-17 22:23:22 +0530

    Changed ad so that it appears at end of the list

--- app/src/main/java/com/example/flobiz_assignment/MainActivity.kt
+++ app/src/main/java/com/example/flobiz_assignment/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.flobiz_assignment
 
 import android.content.Context
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.flobiz_assignment.adapter.ItemAdapter
@@ -11,6 +10,7 @@ import com.google.android.material.card.MaterialCardView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    var crossed = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,20 +18,19 @@ class MainActivity : AppCompatActivity() {
         val recyclerView=findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.adapter= ItemAdapter(this,myDataSet)
         recyclerView.setHasFixedSize(true)
+    }
+    fun isAdCrossed(): Boolean {
         val ad = findViewById<MaterialCardView>(R.id.ad_card)
-        val sharedPref = this?.getPreferences(Context.MODE_PRIVATE) ?: return
+        val sharedPref = this?.getPreferences(Context.MODE_PRIVATE)
         val defaultValue = false
-        val value = sharedPref.getBoolean("crossed",false)
-        if(value)
-            ad.visibility = View.GONE
+        return sharedPref.getBoolean("crossed",false)
+    }
+    fun crossIt(){
         val button = findViewById<FloatingActionButton>(R.id.cross_button)
-        button.setOnClickListener{
-            ad.visibility = View.GONE
-            val sharedPref = this?.getPreferences(Context.MODE_PRIVATE)
-            with (sharedPref.edit()) {
-                putBoolean("crossed",true)
-                apply()
-            }
+        val sharedPref = this?.getPreferences(Context.MODE_PRIVATE)
+        with (sharedPref.edit()) {
+            putBoolean("crossed",true)
+            apply()
         }
     }
 }
--- app/src/main/java/com/example/flobiz_assignment/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/flobiz_assignment/adapter/ItemAdapter.kt
@@ -7,26 +7,56 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.flobiz_assignment.MainActivity
 import com.example.flobiz_assignment.R
 import com.example.flobiz_assignment.model.ListItem
+import com.google.android.material.card.MaterialCardView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ItemAdapter(private val context: Context,
                   private val dataset : List<ListItem>)
-    : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
-    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view){
-        val textView : TextView = view.findViewById(R.id.item_title)
-        val imageView : ImageView = view.findViewById(R.id.item_image)
+    : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    class ItemViewHolderA(private val view: View) : RecyclerView.ViewHolder(view){
+        val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        val adapterLayout= LayoutInflater.from(parent.context)
-            .inflate(R.layout.list_item, parent, false)
-        return ItemViewHolder(adapterLayout)
+    class ItemViewHolderB(private val view: View) : RecyclerView.ViewHolder(view){
+        val ad : MaterialCardView = view.findViewById(R.id.ad_card)
+        val crossButton : FloatingActionButton = view.findViewById(R.id.cross_button)
+    }
+    override fun getItemViewType(position: Int): Int {
+        if(position == dataset.size-1)
+            return 1
+        return 0
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        if(viewType==0) {
+            val adapterLayout = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.list_item, parent, false)
+            return ItemViewHolderA(adapterLayout)
+        }
+        else {
+            val adapterLayout = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.myad_view, parent, false)
+            return ItemViewHolderB(adapterLayout)
+        }
     }
 
-    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        val item = dataset[position]
-        holder.textView.text =context.resources.getString(item.stringResourceId)
-        holder.imageView.setImageResource(item.imageResourceId)
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if(position != dataset.size-1) {
+            val item = dataset[position]
+            (holder as ItemViewHolderA).textView.text = context.resources.getString(item.stringResourceId)+" "+(position+1)
+            holder.imageView.setImageResource(item.imageResourceId)
+        }
+        else {
+            val crossButtonValue = (context as MainActivity).isAdCrossed()
+            if(crossButtonValue)
+                (holder as ItemViewHolderB).ad.visibility = View.GONE
+            (holder as ItemViewHolderB).crossButton.setOnClickListener{
+                holder.ad.visibility = View.GONE
+                (context as MainActivity).crossIt()
+            }
+        }
     }
 
     override fun getItemCount()= dataset.size
